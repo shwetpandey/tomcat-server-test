1685266efd9c9a07cb32def4097bbb612c777aa6
shwetpandey <shwetpandey41@gmail.com>
2022-09-27 11:48:01 +0530

    first commit

---
+++ src/test/java/com/tomcat/TomcatTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
